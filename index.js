95327a1fba8d6ddf580b77da7f4e2b95f695b7fa
Praful Rana <prafulrana@gmail.com>
2015-08-27 12:21:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var app = require('express')();
 app.get('/', function(req, res){
   res.send('Hello World');
 });
+app.post('/', function(req, res){
+  res.send('Hello World');
+});
 
 app.use(require('express-bunyan-logger')());
 
